473e38815f0016ae14eef53bc57830cba1219f7e
Stuart Kushner <stuartkushner@Stuarts-MacBook-Pro.local>
2018-02-05 12:09:32 -0500

    Made changes to HTML and handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Whammy Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Tropical Burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Hot Stuff Burger");
